4542bf01e59f548ebe979adbb48a9859d981d807
Mark LeMoine <thatsgobbles@gmail.com>
2018-09-21 17:33:23 -0700

    Included fix for `Cow` attempts from IRC chat

--- src/metadata/resolver.rs
+++ src/metadata/resolver.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 use std::path::PathBuf;
 use std::collections::HashMap;
-use std::borrow::Cow;
+use std::collections::hash_map::Entry;
 
 use failure::Error;
 
@@ -35,64 +35,37 @@ impl MetaResolver {
         meta_location: MetaLocation,
         cache: &mut HashMap<PathBuf, HashMap<PathBuf, MetaBlock>>,
         force: bool,
-    ) -> Result<Cow<HashMap<PathBuf, MetaBlock>>, Error>
+    ) -> Result<&HashMap<PathBuf, MetaBlock>, Error>
     where
         MR: MetaReader,
         P: AsRef<Path>,
     {
         let meta_path = meta_path.as_ref();
 
-        // Only try reading from cache if not trying to force.
-        if !force {
-            // Check to see if the requested meta path is already cached.
-            if let Some(meta_block) = cache.get(meta_path) {
-                // Return the cached entry.
-                return Ok(Cow::Borrowed(meta_block))
-            }
+        if force {
+            cache.remove(meta_path);
         }
 
-        // Otherwise, actually process the meta file.
-        let result = MetaResolver::process_meta_file::<MR, _>(meta_path, meta_location);
+        let file = match cache.entry(meta_path.to_owned()) {
+            Entry::Occupied(e) => e.into_mut(),
+            Entry::Vacant(e) => e.insert(Self::process_meta_file::<MR, _>(meta_path, meta_location)?),
+        };
 
-        Ok(Cow::Owned(hashmap![]))
+        Ok(file)
     }
 
-    pub fn get_simple_metadata<MR, P, MLS>(
-        target_item_path: P,
-        meta_locations: MLS,
-        opt_cache: Option<&mut HashMap<PathBuf, MetaBlock>>,
-    ) -> Result<Cow<MetaBlock>, Error>
+    pub fn process_item_file_cached<MR, P>(
+        item_path: P,
+        meta_location: MetaLocation,
+        cache: &mut HashMap<PathBuf, HashMap<PathBuf, MetaBlock>>,
+        force: bool,
+    ) -> Result<&MetaBlock, Error>
     where
         MR: MetaReader,
         P: AsRef<Path>,
-        MLS: IntoIterator<Item = MetaLocation>,
     {
-        let target_item_path = target_item_path.as_ref();
-
-        // If we have a cache, check to see if the requested path already has metadata.
-        if let Some(cache) = opt_cache {
-            if let Some(meta_block) = cache.get(target_item_path) {
-                // Return the cached entry.
-                return Ok(Cow::Borrowed(meta_block))
-            }
-        }
-
-        let mut merged_meta_block = MetaBlock::new();
-
-        // If there is no cache provided, or if not found in the cache, read the metadata.
-        // Use the provided meta targets.
-        for meta_location in meta_locations.into_iter() {
-            let target_meta_path = meta_location.get_meta_path(target_item_path)?;
-
-            let meta_structure = MR::from_file(&target_meta_path, meta_location)?;
-
-            // TODO: Need a way to get "path extents": the set of item files governed by this meta file/location.
-
-            // let possible_owned_item_paths = meta_location.get_possible_owned_item_paths
-
-            // let plex_map = MetaPlexer::plex(meta_structure, item_paths)
-        }
-
-        Ok(Cow::Owned(MetaBlock::new()))
+        let meta_path = item_path.as_ref();
+        let processed_meta_file = Self::process_meta_file_cached::<MR, _>(&meta_path, meta_location, cache, force)?;
+        processed_meta_file.get(item_path.as_ref()).ok_or(bail!("item path not found in processed metadata: \"{}\"", item_path.as_ref().to_string_lossy()))
     }
 }
