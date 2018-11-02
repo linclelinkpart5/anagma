9b70f9c4bf53050e5b066870eab30926efe94dfc
Mark LeMoine <thatsgobbles@gmail.com>
2018-11-01 18:07:21 -0700

    Removing `MetaProcessor` struct

--- src/metadata/processor.rs
+++ src/metadata/processor.rs
@@ -41,94 +41,88 @@ impl std::error::Error for Error {
     }
 }
 
-pub struct MetaProcessor;
+pub fn process_meta_file<P>(
+    meta_path: P,
+    meta_location: MetaLocation,
+    meta_format: MetaFormat,
+    selection: &Selection,
+    sort_order: SortOrder,
+) -> Result<HashMap<PathBuf, MetaBlock>, Error>
+where
+    P: AsRef<Path>,
+{
+    let meta_structure = meta_format.read_file(&meta_path, meta_location).map_err(Error::CannotReadMetadata)?;
 
-impl MetaProcessor {
-    pub fn process_meta_file<P>(
-        meta_path: P,
-        meta_location: MetaLocation,
-        meta_format: MetaFormat,
-        selection: &Selection,
-        sort_order: SortOrder,
-    ) -> Result<HashMap<PathBuf, MetaBlock>, Error>
-    where
-        P: AsRef<Path>,
-    {
-        let meta_structure = meta_format.read_file(&meta_path, meta_location).map_err(Error::CannotReadMetadata)?;
+    let selected_item_paths = meta_location.get_selected_item_paths(&meta_path, selection).map_err(Error::CannotFindItemPaths)?;
 
-        let selected_item_paths = meta_location.get_selected_item_paths(&meta_path, selection).map_err(Error::CannotFindItemPaths)?;
+    let mut meta_plexed = hashmap![];
+    for meta_plex_res in MetaPlexer::plex(meta_structure, selected_item_paths, sort_order) {
+        match meta_plex_res {
+            Ok((item_path, mb)) => { meta_plexed.insert(item_path, mb); },
+            Err(e) => { warn!("{}", e); },
+        }
+    }
 
-        let mut meta_plexed = hashmap![];
-        for meta_plex_res in MetaPlexer::plex(meta_structure, selected_item_paths, sort_order) {
-            match meta_plex_res {
-                Ok((item_path, mb)) => { meta_plexed.insert(item_path, mb); },
-                Err(e) => { warn!("{}", e); },
+    Ok(meta_plexed)
+}
+
+pub fn process_item_file<P>(
+    item_path: P,
+    meta_location: MetaLocation,
+    meta_format: MetaFormat,
+    selection: &Selection,
+    sort_order: SortOrder,
+) -> Result<MetaBlock, Error>
+where
+    P: AsRef<Path>,
+{
+    let meta_path = match meta_location.get_meta_path(&item_path) {
+        Err(e) => {
+            match e {
+                LocationError::NonexistentMetaPath(..) |
+                    LocationError::InvalidItemDirPath(..) |
+                    LocationError::NoItemPathParent(..) => { return Ok(MetaBlock::new()); },
+                _ => { return Err(e).map_err(Error::CannotFindMetaPath)?; }
             }
-        }
+        },
+        Ok(p) => p,
+    };
 
-        Ok(meta_plexed)
+    let mut processed_meta_file = process_meta_file(&meta_path, meta_location, meta_format, selection, sort_order)?;
+
+    // The remaining results can be thrown away.
+    if let Some(meta_block) = processed_meta_file.remove(item_path.as_ref()) {
+        Ok(meta_block)
+    }
+    else {
+        Err(Error::MissingMetadata)?
+    }
+}
+
+// Processes multiple locations for a target item at once, merging the results.
+// Merging is "combine-last", so matching result keys for subsequent locations override earlier keys.
+pub fn process_item_file_flattened<P, II>(
+    item_path: P,
+    meta_locations: II,
+    meta_format: MetaFormat,
+    selection: &Selection,
+    sort_order: SortOrder,
+) -> Result<MetaBlock, Error>
+where
+    P: AsRef<Path>,
+    II: IntoIterator<Item = MetaLocation>,
+{
+    let mut comp_mb = MetaBlock::new();
+
+    for meta_location in meta_locations.into_iter() {
+        comp_mb.extend(process_item_file(&item_path, meta_location, meta_format, selection, sort_order)?);
     }
 
-    pub fn process_item_file<P>(
-        item_path: P,
-        meta_location: MetaLocation,
-        meta_format: MetaFormat,
-        selection: &Selection,
-        sort_order: SortOrder,
-    ) -> Result<MetaBlock, Error>
-    where
-        P: AsRef<Path>,
-    {
-        let meta_path = match meta_location.get_meta_path(&item_path) {
-            Err(e) => {
-                match e {
-                    LocationError::NonexistentMetaPath(..) |
-                        LocationError::InvalidItemDirPath(..) |
-                        LocationError::NoItemPathParent(..) => { return Ok(MetaBlock::new()); },
-                    _ => { return Err(e).map_err(Error::CannotFindMetaPath)?; }
-                }
-            },
-            Ok(p) => p,
-        };
-
-        let mut processed_meta_file = Self::process_meta_file(&meta_path, meta_location, meta_format, selection, sort_order)?;
-
-        // The remaining results can be thrown away.
-        if let Some(meta_block) = processed_meta_file.remove(item_path.as_ref()) {
-            Ok(meta_block)
-        }
-        else {
-            Err(Error::MissingMetadata)?
-        }
-    }
-
-    // Processes multiple locations for a target item at once, merging the results.
-    // Merging is "combine-last", so matching result keys for subsequent locations override earlier keys.
-    pub fn process_item_file_flattened<P, II>(
-        item_path: P,
-        meta_locations: II,
-        meta_format: MetaFormat,
-        selection: &Selection,
-        sort_order: SortOrder,
-    ) -> Result<MetaBlock, Error>
-    where
-        P: AsRef<Path>,
-        II: IntoIterator<Item = MetaLocation>,
-    {
-        let mut comp_mb = MetaBlock::new();
-
-        for meta_location in meta_locations.into_iter() {
-            comp_mb.extend(Self::process_item_file(&item_path, meta_location, meta_format, selection, sort_order)?);
-        }
-
-        Ok(comp_mb)
-    }
+    Ok(comp_mb)
 }
 
 #[cfg(test)]
 mod tests {
-    use super::MetaProcessor;
-
     use library::config::Config;
     use metadata::reader::MetaFormat;
     use metadata::location::MetaLocation;
@@ -232,7 +226,7 @@ mod tests {
         for (input, expected) in inputs_and_expected {
             let (meta_path, meta_location) = input;
 
-            let produced = MetaProcessor::process_meta_file(meta_path, meta_location, MetaFormat::Yaml, selection, sort_order).unwrap();
+            let produced = super::process_meta_file(meta_path, meta_location, MetaFormat::Yaml, selection, sort_order).unwrap();
             assert_eq!(expected, produced);
         }
     }
@@ -289,7 +283,7 @@ mod tests {
         for (input, expected) in inputs_and_expected {
             let (item_path, meta_location) = input;
 
-            let produced = MetaProcessor::process_item_file(item_path, meta_location, MetaFormat::Yaml, selection, sort_order).unwrap();
+            let produced = super::process_item_file(item_path, meta_location, MetaFormat::Yaml, selection, sort_order).unwrap();
             assert_eq!(expected, produced);
         }
     }
@@ -341,7 +335,7 @@ mod tests {
         for (input, expected) in inputs_and_expected {
             let item_path = input;
 
-            let produced = MetaProcessor::process_item_file_flattened(item_path, meta_locations.clone(), MetaFormat::Yaml, selection, sort_order).unwrap();
+            let produced = super::process_item_file_flattened(item_path, meta_locations.clone(), MetaFormat::Yaml, selection, sort_order).unwrap();
             assert_eq!(expected, produced);
         }
     }
