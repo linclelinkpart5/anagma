a208da3e98f9888bc79531030d7a4ab3b0ce45d2
Mark LeMoine <thatsgobbles@gmail.com>
2019-01-16 16:32:40 -0800

    Removed old `Fallback.is_ancestor` and `Fallback.is_children` methods and usage

--- src/config/fallback_method/mod.rs
+++ src/config/fallback_method/mod.rs
@@ -53,18 +53,6 @@ impl Default for Fallback {
     }
 }
 
-impl Fallback {
-    pub fn is_ancestor(&self) -> bool {
-        if let Self::Inherit(..) = self { true }
-        else { false }
-    }
-
-    pub fn is_children(&self) -> bool {
-        if let Self::Harvest(..) = self { true }
-        else { false }
-    }
-}
-
 /// Node type for the tree representation of fallback methods.
 #[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
 #[serde(untagged)]
@@ -86,10 +74,6 @@ pub fn process_fallbacks<P: AsRef<Path>>(
     default_fallback: Fallback,
 ) -> MetaBlock
 {
-    // Check which directions we need to traverse, if any.
-    let need_ancestor = default_fallback.is_ancestor();
-    let need_children = default_fallback.is_children();
-
     // Load the origin metadata.
     // This is the isolated metadata block for just the starting item.
     let origin_mb = MetaProcessor::process_item_file(
