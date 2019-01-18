b8c50e4c277bc01c2cfe44f099c2ba7d95be53fc
Mark LeMoine <thatsgobbles@gmail.com>
2019-01-18 11:36:08 -0800

    Created `listify_fallback_spec` and tests

--- src/config/fallback_method/mod.rs
+++ src/config/fallback_method/mod.rs
@@ -58,12 +58,43 @@ impl Default for Fallback {
 #[serde(untagged)]
 pub enum FallbackSpecNode {
     Leaf(Fallback),
-    Pass(HashMap<String, FallbackSpecNode>),
-    Both(Fallback, HashMap<String, FallbackSpecNode>),
+    Pass(FallbackSpec),
+    Both(Fallback, FallbackSpec),
 }
 
 pub type FallbackSpec = HashMap<String, FallbackSpecNode>;
 
+fn listify_fallback_spec(fallback_spec: &FallbackSpec) -> HashMap<Vec<&String>, Fallback> {
+    let mut mapping = HashMap::new();
+
+    listify_fallback_spec_helper(fallback_spec, vec![], &mut mapping);
+
+    mapping
+}
+
+fn listify_fallback_spec_helper<'a>(
+    fallback_spec: &'a FallbackSpec,
+    curr_path: Vec<&'a String>,
+    mapping: &mut HashMap<Vec<&'a String>, Fallback>)
+{
+    for (k, fsn) in fallback_spec {
+        let mut new_path = curr_path.clone();
+        new_path.push(k);
+
+        match fsn {
+            FallbackSpecNode::Leaf(fb) => {
+                mapping.insert(new_path, *fb);
+            },
+            FallbackSpecNode::Pass(sub_fbs) => {
+                listify_fallback_spec_helper(sub_fbs, new_path, mapping);
+            },
+            FallbackSpecNode::Both(fb, sub_fbs) => {
+                mapping.insert(new_path.clone(), *fb);
+                listify_fallback_spec_helper(sub_fbs, new_path, mapping);
+            },
+        }
+    }
+}
 
 pub fn process_fallbacks<P: AsRef<Path>, S: AsRef<str>>(
     start_item_path: P,
@@ -86,4 +117,60 @@ pub fn process_fallbacks<P: AsRef<Path>, S: AsRef<str>>(
     ).unwrap();
 
     MetaBlock::new()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Fallback;
+    use super::FallbackSpec;
+    use super::FallbackSpecNode;
+    use super::InheritFallback;
+    use super::HarvestFallback;
+
+    #[test]
+    fn test_listify_fallback_spec() {
+        let title_key = String::from("title");
+        let rg_key = String::from("rg");
+        let peak_key = String::from("peak");
+
+        let inputs_and_expected = vec![
+            (
+                hashmap![],
+                hashmap![],
+            ),
+            (
+                hashmap![
+                    title_key.clone() => FallbackSpecNode::Leaf(Fallback::Inherit(InheritFallback::Override)),
+                ],
+                hashmap![
+                    vec![&title_key] =>
+                        Fallback::Inherit(InheritFallback::Override),
+                ],
+            ),
+            (
+                hashmap![
+                    title_key.clone() => FallbackSpecNode::Leaf(Fallback::Inherit(InheritFallback::Override)),
+                    rg_key.clone() => FallbackSpecNode::Both(
+                        Fallback::Inherit(InheritFallback::Merge),
+                        hashmap![
+                            peak_key.clone() => FallbackSpecNode::Leaf(Fallback::Harvest(HarvestFallback::First)),
+                        ],
+                    ),
+                ],
+                hashmap![
+                    vec![&title_key] =>
+                        Fallback::Inherit(InheritFallback::Override),
+                    vec![&rg_key] =>
+                        Fallback::Inherit(InheritFallback::Merge),
+                    vec![&rg_key, &peak_key] =>
+                        Fallback::Harvest(HarvestFallback::First),
+                ],
+            ),
+        ];
+
+        for (input, expected) in inputs_and_expected {
+            let produced = super::listify_fallback_spec(&input);
+            assert_eq!(expected, produced);
+        }
+    }
 }
