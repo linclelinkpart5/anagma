bb0483b48ccf1473bd904cebc544f93b8954719a
Mark LeMoine <thatsgobbles@gmail.com>
2019-03-12 14:55:36 -0700

    Creating `Error` type for `iter`, and fleshing out iteration process

--- src/config/fallback_method/iter.rs
+++ src/config/fallback_method/iter.rs
@@ -1,5 +1,30 @@
+use std::path::Path;
+
+use config::selection::Selection;
+use config::sort_order::SortOrder;
+use config::meta_format::MetaFormat;
 use metadata::types::MetaKey;
 use metadata::types::MetaVal;
+use metadata::processor::MetaProcessor;
+
+
+#[derive(Debug)]
+pub enum Error {
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+        }
+    }
+}
 
 #[derive(Copy, Clone, Debug)]
 pub enum FallbackIterKind {
@@ -8,15 +33,39 @@ pub enum FallbackIterKind {
     ChildrenBreadth,
 }
 
-pub struct FallbackIter<'k> {
+pub struct PIter<'k, 'p, 's, 'mrk> {
     target_key_path: Vec<&'k MetaKey>,
-    kind: FallbackIterKind,
+    next_path: Option<&'p Path>,
+    meta_format: MetaFormat,
+    selection: &'s Selection,
+    sort_order: SortOrder,
+    map_root_key: &'mrk str,
 }
 
-impl<'k> Iterator for FallbackIter<'k> {
-    type Item = MetaVal;
+impl<'k, 'p, 's, 'mrk> Iterator for PIter<'k, 'p, 's, 'mrk> {
+    type Item = Result<MetaVal, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        Some(MetaVal::Nil)
+        match self.next_path {
+            Some(curr_path) => {
+                self.next_path = curr_path.parent();
+
+                let processed = MetaProcessor::process_item_file(
+                    curr_path,
+                    self.meta_format,
+                    self.selection,
+                    self.sort_order,
+                    self.map_root_key,
+                );
+
+                match processed {
+                    Ok(mb) => {},
+                    Err(err) => {},
+                };
+
+                None
+            },
+            None => None,
+        }
     }
 }
