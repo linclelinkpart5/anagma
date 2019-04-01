59f744959d024ec4a2b5623f8ec36fc82da82ffe
Mark LeMoine <thatsgobbles@gmail.com>
2019-04-01 16:53:39 -0700

    More `FieldConsumer` variants and logic

--- src/metadata/consumer/mod.rs
+++ src/metadata/consumer/mod.rs
@@ -2,6 +2,8 @@ use metadata::types::MetaVal;
 use metadata::producer::field::SimpleMetaFieldProducer;
 use metadata::producer::field::Error as FieldProducerError;
 
+use itertools::Itertools;
+
 #[derive(Debug)]
 pub enum Error {
     FieldProducer(FieldProducerError),
@@ -29,21 +31,61 @@ impl From<FieldProducerError> for Error {
     }
 }
 
+/// Methods for processing and fetching useful data from field producers.
+/// Unless specified, all methods only operate on valid values, and ignore errors.
 pub enum FieldConsumer {
+    /// Return all values from the producer.
+    Collect,
+    /// Count the number of values from the producer.
+    Count,
     /// Return the first found value from the producer.
     First,
     /// Return the last found value from the producer.
     Last,
-    /// Return all values from the producer.
-    Collect,
+    /// Flattens any sequence values from the producer, leaving other meta values unchanged.
+    Flatten,
+    /// Filters out values that have already been produced once.
+    Unique,
+    /// Equals true if all the values in the producer are equal to each other.
+    AllEqual,
 }
 
 impl FieldConsumer {
     pub fn process(&self, field_producer: &mut SimpleMetaFieldProducer) -> MetaVal {
         match self {
+            &Self::Collect => MetaVal::Seq(field_producer.collect()),
+            &Self::Count => MetaVal::Int(field_producer.count() as i64),
             &Self::First => field_producer.next().unwrap_or_else(|| MetaVal::Nil),
             &Self::Last => field_producer.last().unwrap_or_else(|| MetaVal::Nil),
-            &Self::Collect => MetaVal::Seq(field_producer.collect()),
+            &Self::Flatten => {
+                let mut flat = vec![];
+
+                for mv in field_producer {
+                    match mv {
+                        MetaVal::Seq(seq) => flat.extend(seq.into_iter()),
+                        o => flat.push(o),
+                    }
+                }
+
+                MetaVal::Seq(flat)
+            },
+            &Self::Unique => MetaVal::Seq(field_producer.unique().collect()),
+            &Self::AllEqual => {
+                MetaVal::Bul(
+                    match field_producer.next() {
+                        None => true,
+                        Some(first_val) => {
+                            while let Some(next_val) = field_producer.next() {
+                                if first_val != next_val {
+                                    return MetaVal::Bul(false);
+                                }
+                            }
+
+                            true
+                        }
+                    }
+                )
+            }
         }
     }
 }
