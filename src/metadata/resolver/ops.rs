72a52ca428edc7b0a916fb1cdf4e7acce5f786a7
Mark LeMoine <thatsgobbles@gmail.com>
2019-04-03 16:51:42 -0700

    Working on `ops` module, and `pop*` methods on `OperandStack`

--- src/metadata/resolver/ops.rs
+++ src/metadata/resolver/ops.rs
@@ -1,5 +1,31 @@
 use metadata::resolver::streams::Stream;
 use metadata::types::MetaVal;
+use metadata::resolver::iterable_like::IterableLike;
+use metadata::resolver::number_like::NumberLike;
+
+#[derive(Debug)]
+pub enum Error {
+    UnexpectedOperand,
+    EmptyStack,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Self::UnexpectedOperand => write!(f, "unexpected operand on stack"),
+            Self::EmptyStack => write!(f, "empty operand stack"),
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match *self {
+            Self::UnexpectedOperand => None,
+            Self::EmptyStack => None,
+        }
+    }
+}
 
 pub enum Operand<'k, 'p, 's> {
     Stream(Stream<'k, 'p, 's>),
@@ -8,8 +34,24 @@ pub enum Operand<'k, 'p, 's> {
 
 pub struct OperandStack<'k, 'p, 's>(Vec<Operand<'k, 'p, 's>>);
 
-#[derive(Copy, Clone, Debug)]
-pub enum NullaryOp {
-    Parents,
-    Children,
+impl<'k, 'p, 's> OperandStack<'k, 'p, 's> {
+    pub fn pop(&mut self) -> Result<Operand, Error> {
+        self.0.pop().ok_or_else(|| Error::EmptyStack)
+    }
+
+    pub fn pop_iterable_like(&mut self) -> Result<IterableLike, Error> {
+        match self.pop()? {
+            Operand::Stream(s) => Ok(IterableLike::Stream(s)),
+            Operand::Value(MetaVal::Seq(s)) => Ok(IterableLike::Sequence(s)),
+            _ => Err(Error::UnexpectedOperand),
+        }
+    }
+
+    pub fn pop_number_like(&mut self) -> Result<NumberLike, Error> {
+        match self.pop()? {
+            Operand::Value(MetaVal::Int(i)) => Ok(NumberLike::Integer(i)),
+            Operand::Value(MetaVal::Dec(d)) => Ok(NumberLike::Decimal(d)),
+            _ => Err(Error::UnexpectedOperand),
+        }
+    }
 }
