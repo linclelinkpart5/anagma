5a02513feda92fb2ba1c7178047021edaaa0b38c
Mark LeMoine <thatsgobbles@gmail.com>
2019-04-02 23:34:46 -0700

    Added `Flatten` and `Dedup` streams

--- src/metadata/consumer/streams.rs
+++ src/metadata/consumer/streams.rs
@@ -6,31 +6,69 @@ use metadata::producer::value::SimpleMetaValueProducer;
 /// A stream is a generalization of the different kinds of lazy sequences that can be used/produced by consumers.
 pub enum Stream<'k, 'p, 's> {
     Raw(SimpleMetaValueProducer<'k, 'p, 's>),
-    Flatten,
-    Dedup,
+    Flatten(FlattenStream<'k, 'p, 's>),
+    Dedup(DedupStream<'k, 'p, 's>),
     Unique,
 }
 
-// struct FlattenStream<'k, 'p, 's>(Stream<'k, 'p, 's>, VecDeque<MetaVal>);
+impl<'k, 'p, 's> Iterator for Stream<'k, 'p, 's> {
+    type Item = MetaVal;
 
-// impl<'k, 'p, 's> Iterator for FlattenStream<'k, 'p, 's> {
-//     type Item = MetaVal;
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            &mut Self::Raw(ref mut it) => it.next(),
+            &mut Self::Flatten(ref mut it) => it.next(),
+            &mut Self::Dedup(ref mut it) => it.next(),
+            &mut Self::Unique => None,
+        }
+    }
+}
 
-//     fn next(&mut self) -> Option<Self::Item> {
-//         match self.1.pop_front() {
-//             Some(mv) => Some(mv),
-//             None => {
-//                 // Try to get the next item from the stream.
-//                 match self.0.next() {
-//                     None => None,
-//                     Some(mv) => {
-//                         // Check if this new value needs flattening.
-//                         match mv {
-//                             MetaVal::Seq(seq) => {},
-//                         }
-//                     }
-//                 }
-//             },
-//         }
-//     }
-// }
+pub struct FlattenStream<'k, 'p, 's>(Box<Stream<'k, 'p, 's>>, VecDeque<MetaVal>);
+
+impl<'k, 'p, 's> Iterator for FlattenStream<'k, 'p, 's> {
+    type Item = MetaVal;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.1.pop_front() {
+            Some(mv) => Some(mv),
+            None => {
+                // Try to get the next item from the stream.
+                match self.0.next() {
+                    None => None,
+                    Some(mv) => {
+                        // Check if this new value needs flattening.
+                        match mv {
+                            MetaVal::Seq(seq) => {
+                                // Move all elements in the sequence into the queue.
+                                self.1.extend(seq);
+                                self.next()
+                            },
+                            mv => Some(mv),
+                        }
+                    }
+                }
+            },
+        }
+    }
+}
+
+pub struct DedupStream<'k, 'p, 's>(Box<Stream<'k, 'p, 's>>, Option<MetaVal>);
+
+impl<'k, 'p, 's> Iterator for DedupStream<'k, 'p, 's> {
+    type Item = MetaVal;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let curr_val = self.0.next()?;
+
+        if Some(&curr_val) != self.1.as_ref() {
+            // A non-duplicate was found.
+            self.1 = Some(curr_val.clone());
+            Some(curr_val)
+        }
+        else {
+            // Delegate to the next call.
+            self.next()
+        }
+    }
+}
