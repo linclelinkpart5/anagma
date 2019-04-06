a533e295fd11d572afd567768651222ca8be2e79
Mark LeMoine <thatsgobbles@gmail.com>
2019-04-06 12:02:18 -0700

    Same treatment of enums -> traits for `file_walker` module

--- src/util/file_walkers.rs
+++ src/util/file_walkers.rs
@@ -27,52 +27,8 @@ impl std::error::Error for Error {
     }
 }
 
-/// Generic walker that supports either visiting parent or child files of an origin path.
-pub enum FileWalker<'p> {
-    Parent(ParentFileWalker<'p>),
-    Child(ChildFileWalker<'p>),
-}
-
-impl<'p> Iterator for FileWalker<'p> {
-    type Item = Result<Cow<'p, Path>, Error>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self {
-            // Parent walkers cannot error, so this needs wrapping in a `Result`.
-            &mut Self::Parent(ref mut fw) => fw.next().map(Result::Ok),
-            &mut Self::Child(ref mut fw) => fw.next(),
-        }
-    }
-}
-
-impl<'p> FileWalker<'p> {
-    pub fn delve(&mut self, selection: &Selection, sort_order: SortOrder) -> Result<(), Error> {
-        match self {
-            // Parent walkers do not have to delve, just no-op.
-            &mut Self::Parent(..) => Ok(()),
-            &mut Self::Child(ref mut fw) => fw.delve(selection, sort_order),
-        }
-    }
-
-    pub fn new_parent_walker(origin_item_path: &'p Path) -> Self {
-        ParentFileWalker::new(origin_item_path).into()
-    }
-
-    pub fn new_child_walker(origin_item_path: &'p Path) -> Self {
-        ChildFileWalker::new(origin_item_path).into()
-    }
-}
-
-impl<'p> From<ParentFileWalker<'p>> for FileWalker<'p> {
-    fn from(fw: ParentFileWalker<'p>) -> Self {
-        Self::Parent(fw)
-    }
-}
-
-impl<'p> From<ChildFileWalker<'p>> for FileWalker<'p> {
-    fn from(fw: ChildFileWalker<'p>) -> Self {
-        Self::Child(fw)
-    }
+pub trait FW<'p>: Iterator<Item = Result<Cow<'p, Path>, Error>> {
+    fn spelunk(&mut self, selection: &Selection, sort_order: SortOrder) -> Result<(), Error>;
 }
 
 pub struct ParentFileWalker<'p>(Option<&'p Path>);
@@ -84,7 +40,7 @@ impl<'p> ParentFileWalker<'p> {
 }
 
 impl<'p> Iterator for ParentFileWalker<'p> {
-    type Item = Cow<'p, Path>;
+    type Item = Result<Cow<'p, Path>, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.0 {
@@ -93,10 +49,16 @@ impl<'p> Iterator for ParentFileWalker<'p> {
 
                 self.0 = p.parent();
 
-                ret.map(Cow::Borrowed)
+                ret.map(Cow::Borrowed).map(Result::Ok)
             },
             None => None,
         }
+    }
+}
+
+impl<'p> FW<'p> for ParentFileWalker<'p> {
+    fn spelunk(&mut self, _selection: &Selection, _sort_order: SortOrder) -> Result<(), Error> {
+        Ok(())
     }
 }
 
@@ -159,6 +121,77 @@ impl<'p> Iterator for ChildFileWalker<'p> {
     }
 }
 
+impl<'p> FW<'p> for ChildFileWalker<'p> {
+    fn spelunk(&mut self, selection: &Selection, sort_order: SortOrder) -> Result<(), Error> {
+        // Manually delves into a directory, and adds its subitems to the frontier.
+        if let Some(lpp) = self.last_processed_path.take() {
+            // If the last processed path is a directory, add its children to the frontier.
+            if lpp.is_dir() {
+                match selection.select_in_dir_sorted(&lpp, sort_order) {
+                    Err(err) => {
+                        return Err(Error::Selection(err));
+                    },
+                    Ok(mut sub_item_paths) => {
+                        // NOTE: Reversing and pushing onto the front of the queue is needed.
+                        for p in sub_item_paths.drain(..).rev() {
+                            self.frontier.push_front(Ok(Cow::Owned(p)));
+                        }
+                    },
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+/// Generic walker that supports either visiting parent or child files of an origin path.
+pub enum FileWalker<'p> {
+    Parent(ParentFileWalker<'p>),
+    Child(ChildFileWalker<'p>),
+}
+
+impl<'p> Iterator for FileWalker<'p> {
+    type Item = Result<Cow<'p, Path>, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            &mut Self::Parent(ref mut fw) => fw.next(),
+            &mut Self::Child(ref mut fw) => fw.next(),
+        }
+    }
+}
+
+impl<'p> FileWalker<'p> {
+    pub fn delve(&mut self, selection: &Selection, sort_order: SortOrder) -> Result<(), Error> {
+        match self {
+            // Parent walkers do not have to delve, just no-op.
+            &mut Self::Parent(..) => Ok(()),
+            &mut Self::Child(ref mut fw) => fw.delve(selection, sort_order),
+        }
+    }
+
+    pub fn new_parent_walker(origin_item_path: &'p Path) -> Self {
+        ParentFileWalker::new(origin_item_path).into()
+    }
+
+    pub fn new_child_walker(origin_item_path: &'p Path) -> Self {
+        ChildFileWalker::new(origin_item_path).into()
+    }
+}
+
+impl<'p> From<ParentFileWalker<'p>> for FileWalker<'p> {
+    fn from(fw: ParentFileWalker<'p>) -> Self {
+        Self::Parent(fw)
+    }
+}
+
+impl<'p> From<ChildFileWalker<'p>> for FileWalker<'p> {
+    fn from(fw: ChildFileWalker<'p>) -> Self {
+        Self::Child(fw)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::ParentFileWalker;
@@ -176,10 +209,10 @@ mod tests {
         let start_path = root_dir.path().join("0").join("0_1").join("0_1_0");
         let mut walker = ParentFileWalker::new(&start_path);
 
-        assert_eq!(walker.next().unwrap(), root_dir.path().join("0").join("0_1").join("0_1_0"));
-        assert_eq!(walker.next().unwrap(), root_dir.path().join("0").join("0_1"));
-        assert_eq!(walker.next().unwrap(), root_dir.path().join("0"));
-        assert_eq!(walker.next().unwrap(), root_dir.path());
+        assert_eq!(walker.next().unwrap().unwrap(), root_dir.path().join("0").join("0_1").join("0_1_0"));
+        assert_eq!(walker.next().unwrap().unwrap(), root_dir.path().join("0").join("0_1"));
+        assert_eq!(walker.next().unwrap().unwrap(), root_dir.path().join("0"));
+        assert_eq!(walker.next().unwrap().unwrap(), root_dir.path());
     }
 
     #[test]
