c7168f24bd98857570c4ef57b77cfb658d2924ba
Mark LeMoine <thatsgobbles@gmail.com>
2019-05-01 18:33:01 -0700

    Ready to start plugging in `UnaryOp`

--- src/functions/op/operator.rs
+++ src/functions/op/operator.rs
@@ -5,7 +5,6 @@ use std::convert::TryFrom;
 use itertools::Itertools;
 
 use crate::functions::op::operand::Operand;
-use crate::functions::util::IterableLike;
 use crate::functions::util::NumberLike;
 use crate::functions::util::StreamAdaptor;
 use crate::functions::Error;
@@ -63,9 +62,6 @@ impl UnaryPredicate {
 
 #[derive(Clone, Copy, Debug)]
 pub enum UnaryConverter {
-    // All unary predicates can be treated as unary converters.
-    Predicate(UnaryPredicate),
-
     Count,
     First,
     Last,
@@ -78,13 +74,12 @@ pub enum UnaryConverter {
     Flatten,
     Dedup,
     Unique,
+    AllEqual,
 }
 
 impl UnaryConverter {
     pub fn process<'mv>(&self, mv: MetaVal<'mv>) -> Result<MetaVal<'mv>, Error> {
         match self {
-            &Self::Predicate(pred) => pred.process(&mv).map(MetaVal::Bul),
-
             &Self::Count => {
                 let ref_seq: &Vec<_> = (&mv).try_into()?;
                 Ok(MetaVal::Int(ref_seq.len() as i64))
@@ -146,6 +141,7 @@ impl UnaryConverter {
                 // TODO: Figure out equality rules.
                 Ok(MetaVal::Seq(seq.into_iter().unique().collect()))
             },
+            &Self::AllEqual => UnaryPredicate::AllEqual.process(&mv).map(MetaVal::Bul),
         }
     }
 }
@@ -254,142 +250,47 @@ impl UnaryIterConsumer {
     }
 }
 
+// All predicates are converters.
+impl From<UnaryPredicate> for UnaryConverter {
+    fn from(p: UnaryPredicate) -> Self {
+        match p {
+            UnaryPredicate::AllEqual => Self::AllEqual,
+        }
+    }
+}
+
+impl TryFrom<UnaryConverter> for UnaryPredicate {
+    type Error = Error;
+
+    fn try_from(p: UnaryConverter) -> Result<Self, Self::Error> {
+        match p {
+            UnaryConverter::AllEqual => Ok(Self::AllEqual),
+            _ => Err(Error::NotPredicate),
+        }
+    }
+}
+
 #[derive(Clone, Copy, Debug)]
 pub enum UnaryOp {
-    // (Iterable<V>) -> Sequence<V>
     Collect,
-    // (Iterable<V>) -> Integer
     Count,
-    // (Iterable<V>) -> V
     First,
-    // (Iterable<V>) -> V
     Last,
-    // (Iterable<Number>) -> Number
     MaxIn,
-    // (Iterable<Number>) -> Number
     MinIn,
-    // (Iterable<V>) -> Sequence<V>
     Rev,
-    // (Iterable<Number>) -> Number
     Sum,
-    // (Iterable<Number>) -> Number
     Product,
-    // (Iterable<V>) -> Boolean
     AllEqual,
-    // (Iterable<V>) -> Sequence<V>
     Sort,
-
-    // (Sequence<V>) -> Sequence<V>
-    // (Stream<V>) -> Stream<V>
     Flatten,
-    // (Sequence<V>) -> Sequence<V>
-    // (Stream<V>) -> Stream<V>
     Dedup,
-    // (Sequence<V>) -> Sequence<V>
-    // (Stream<V>) -> Stream<V>
     Unique,
 }
 
 impl UnaryOp {
     pub fn process<'o>(&self, operand: Operand<'o>) -> Result<Operand<'o>, Error> {
-        match self {
-            &Self::Collect => {
-                match operand {
-                    Operand::Value(mv) => {
-                        match mv.into_owned() {
-                            MetaVal::Seq(seq) => Ok(Operand::Value(Cow::Owned(MetaVal::Seq(seq)))),
-                            _ => Err(Error::NotSequence),
-                        }
-                    },
-                    Operand::StreamAdaptor(sa) => sa.collect::<Result<Vec<_>, _>>().map(MetaVal::Seq).map(Cow::Owned).map(Operand::Value),
-                    _ => Err(Error::InvalidOperand),
-                }
-            },
-            &Self::Count => {
-                // Only a reference is needed here for sequences.
-                match operand {
-                    Operand::Value(ref mv) => {
-                        match mv.as_ref() {
-                            &MetaVal::Seq(ref seq) => Ok(Operand::Value(Cow::Owned(MetaVal::Int(seq.len() as i64)))),
-                            _ => Err(Error::NotSequence),
-                        }
-                    },
-                    Operand::StreamAdaptor(sa) => {
-                        let mut c: usize = 0;
-
-                        for res_mv in sa {
-                            res_mv?;
-                            c += 1;
-                        }
-
-                        Ok(Operand::Value(Cow::Owned(MetaVal::Int(c as i64))))
-                    },
-                    _ => Err(Error::InvalidOperand),
-                }
-            },
-            &Self::First => {
-                match operand {
-                    Operand::Value(mv) => {
-                        match mv.into_owned() {
-                            MetaVal::Seq(seq) => {
-                                seq.into_iter().next().map(Cow::Owned).map(Operand::Value).ok_or(Error::EmptySequence)
-                            },
-                            _ => Err(Error::NotSequence),
-                        }
-                    },
-                    Operand::StreamAdaptor(mut sa) => {
-                        sa.next().ok_or(Error::EmptyStream)?.map(Cow::Owned).map(Operand::Value)
-                    },
-                    _ => Err(Error::InvalidOperand),
-                }
-            },
-            &Self::Last => {
-                match operand {
-                    Operand::Value(mv) => {
-                        match mv.into_owned() {
-                            MetaVal::Seq(seq) => {
-                                seq.into_iter().last().map(Cow::Owned).map(Operand::Value).ok_or(Error::EmptySequence)
-                            },
-                            _ => Err(Error::NotSequence),
-                        }
-                    },
-                    Operand::StreamAdaptor(sa) => {
-                        let mut last = None;
-                        for res_mv in sa {
-                            last = Some(res_mv?);
-                        }
-                        last.ok_or(Error::EmptyStream).map(Cow::Owned).map(Operand::Value)
-                    },
-                    _ => Err(Error::InvalidOperand),
-                }
-            },
-            &Self::MaxIn | &Self::MinIn => {
-                let mut m: Option<NumberLike> = None;
-
-                let il: IterableLike<'_> = operand.try_into()?;
-
-                for mv in il {
-                    let num: NumberLike = mv?.try_into()?;
-
-                    m = Some(
-                        match m {
-                            None => num,
-                            Some(curr_m) => {
-                                match self {
-                                    &Self::MaxIn => curr_m.max(num),
-                                    &Self::MinIn => curr_m.min(num),
-                                    _ => unreachable!(),
-                                }
-                            },
-                        }
-                    );
-                }
-
-                Ok(Operand::Value(Cow::Owned(m.ok_or(Error::EmptyIterable)?.into())))
-            },
-            // Not finished yet!
-            _ => Ok(Operand::Value(Cow::Owned(MetaVal::Nil))),
-        }
+        Err(Error::InvalidOperand)
     }
 }
 
