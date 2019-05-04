49ada0b667eb136abd845d3719ed2e9191049eea
Mark LeMoine <thatsgobbles@gmail.com>
2019-05-04 16:16:15 -0400

    Creating tests for `Predicate`

--- src/functions/operator/unary/predicate.rs
+++ src/functions/operator/unary/predicate.rs
@@ -30,3 +30,48 @@ impl Predicate {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Predicate;
+
+    use crate::metadata::types::MetaVal;
+    use crate::functions::Error;
+
+    #[test]
+    fn test_process() {
+        let mv_ne = MetaVal::Seq(vec![
+            MetaVal::Int(1),
+            MetaVal::Int(2),
+            MetaVal::Int(3),
+        ]);
+        let mv_eq = MetaVal::Seq(vec![
+            MetaVal::Int(1),
+            MetaVal::Int(1),
+            MetaVal::Int(1),
+        ]);
+
+        let inputs_and_expected = vec![
+            ((Predicate::AllEqual, &mv_ne), false),
+            ((Predicate::AllEqual, &mv_eq), true),
+        ];
+
+        for (inputs, expected) in inputs_and_expected {
+            let (pred, seq) = inputs;
+            let produced = pred.process(seq).unwrap();
+            assert_eq!(expected, produced);
+        }
+
+        let mv_bad = MetaVal::Nil;
+        let not_sequence_err_cases = vec![
+            (Predicate::AllEqual, &mv_bad),
+        ];
+
+        for (pred, mv) in not_sequence_err_cases {
+            match pred.process(mv) {
+                Err(Error::NotSequence) => {},
+                _ => panic!("expected a failure case"),
+            }
+        }
+    }
+}
