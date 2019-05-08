2925281e6622a7b50bb5a15583240b3845e3b210
Mark LeMoine <thatsgobbles@gmail.com>
2019-05-07 21:29:55 -0700

    Created some simple binary predicates

--- src/functions/operator/binary/predicate.rs
+++ src/functions/operator/binary/predicate.rs
@@ -5,11 +5,23 @@ use crate::functions::Error;
 
 #[derive(Clone, Copy, Debug)]
 pub enum Predicate {
+    Eq,
+    Ne,
+    Lt,
+    Le,
+    Gt,
+    Ge,
 }
 
 impl Predicate {
-    pub fn process<'mv>(&self, mv: &'mv MetaVal<'mv>) -> Result<bool, Error> {
+    pub fn process<'mv>(&self, mv_a: &'mv MetaVal<'mv>, mv_b: &'mv MetaVal<'mv>) -> Result<bool, Error> {
         match self {
+            &Self::Eq => Ok(mv_a == mv_b),
+            &Self::Ne => Ok(mv_a != mv_b),
+            &Self::Lt => Ok(mv_a < mv_b),
+            &Self::Le => Ok(mv_a <= mv_b),
+            &Self::Gt => Ok(mv_a > mv_b),
+            &Self::Ge => Ok(mv_a >= mv_b),
             _ => Ok(false),
         }
     }
