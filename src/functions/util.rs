00ba97d548e8cba264bd5b607f3d91016399d2de
Mark LeMoine <thatsgobbles@gmail.com>
2019-05-15 16:57:11 -0700

    Beginning centralizing implementation of all ops in `functions::util`

--- src/functions/util.rs
+++ src/functions/util.rs
@@ -4,3 +4,15 @@ pub mod stream_adaptor;
 pub use self::number_like::NumberLike;
 pub use self::stream_adaptor::StreamAdaptor;
 pub use self::stream_adaptor::*;
+
+use crate::metadata::types::MetaVal;
+use crate::functions::Error;
+
+/// Namespace for all the implementation of various functions in this module.
+pub struct Impl;
+
+impl Impl {
+    pub fn collect(sa: StreamAdaptor) -> Result<Vec<MetaVal>, Error> {
+        Ok(sa.collect::<Result<Vec<_>, _>>()?)
+    }
+}
