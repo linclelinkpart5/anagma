885dd82b154fe4378adba8febe079775e263264d
Mark LeMoine <thatsgobbles@gmail.com>
2019-05-15 18:06:50 -0700

    More impls for ops

--- src/functions/util.rs
+++ src/functions/util.rs
@@ -5,8 +5,16 @@ pub use self::number_like::NumberLike;
 pub use self::stream_adaptor::StreamAdaptor;
 pub use self::stream_adaptor::*;
 
+use std::convert::TryInto;
+
 use crate::metadata::types::MetaVal;
 use crate::functions::Error;
+
+#[derive(Clone, Copy)]
+enum MinMax { Min, Max, }
+
+#[derive(Clone, Copy)]
+enum SumProd { Sum, Prod, }
 
 /// Namespace for all the implementation of various functions in this module.
 pub struct Impl;
@@ -15,4 +23,91 @@ impl Impl {
     pub fn collect(sa: StreamAdaptor) -> Result<Vec<MetaVal>, Error> {
         Ok(sa.collect::<Result<Vec<_>, _>>()?)
     }
+
+    pub fn count(sa: StreamAdaptor) -> Result<usize, Error> {
+        let mut c: usize = 0;
+        for res_mv in sa { res_mv?; c += 1; }
+        Ok(c)
+    }
+
+    pub fn first(sa: StreamAdaptor) -> Result<MetaVal, Error> {
+        sa.into_iter().next().ok_or(Error::EmptyStream)?
+    }
+
+    pub fn last(sa: StreamAdaptor) -> Result<MetaVal, Error> {
+        let mut last = None;
+        for res_mv in sa { last = Some(res_mv?); }
+        last.ok_or(Error::EmptyStream)
+    }
+
+    fn min_max(sa: StreamAdaptor, flag: MinMax) -> Result<NumberLike, Error> {
+        let mut sa = sa.into_iter();
+        match sa.next() {
+            None => Err(Error::EmptySequence),
+            Some(first_res_mv) => {
+                let mut target_nl: NumberLike = first_res_mv?.try_into()?;
+
+                for res_mv in sa {
+                    let nl: NumberLike = res_mv?.try_into()?;
+                    target_nl = match flag {
+                        MinMax::Min => target_nl.min(nl),
+                        MinMax::Max => target_nl.max(nl),
+                    };
+                }
+
+                Ok(target_nl)
+            }
+        }
+    }
+
+    pub fn min(sa: StreamAdaptor) -> Result<NumberLike, Error> {
+        Self::min_max(sa, MinMax::Min)
+    }
+
+    pub fn max(sa: StreamAdaptor) -> Result<NumberLike, Error> {
+        Self::min_max(sa, MinMax::Max)
+    }
+
+    pub fn rev(sa: StreamAdaptor) -> Result<Vec<MetaVal>, Error> {
+        let mut seq = Self::collect(sa)?;
+        seq.reverse();
+        Ok(seq)
+    }
+
+    pub fn sort(sa: StreamAdaptor) -> Result<Vec<MetaVal>, Error> {
+        let mut seq = Self::collect(sa)?;
+        // TODO: Use proper sort by key.
+        seq.sort();
+        Ok(seq)
+    }
+
+    fn sum_prod(sa: StreamAdaptor, flag: SumProd) -> Result<NumberLike, Error> {
+        let mut total = match flag {
+            SumProd::Sum => NumberLike::Integer(0),
+            SumProd::Prod => NumberLike::Integer(1),
+        };
+
+        for res_mv in sa {
+            let nl: NumberLike = res_mv?.try_into()?;
+
+            match flag {
+                SumProd::Sum => { total += nl; },
+                SumProd::Prod => { total *= nl; },
+            };
+        }
+
+        Ok(total)
+    }
+
+    pub fn sum(sa: StreamAdaptor) -> Result<NumberLike, Error> {
+        Self::sum_prod(sa, SumProd::Sum)
+    }
+
+    pub fn prod(sa: StreamAdaptor) -> Result<NumberLike, Error> {
+        Self::sum_prod(sa, SumProd::Prod)
+    }
+
+    // pub fn flatten(sa: StreamAdaptor) -> Result<StreamAdaptor, Error> {}
+    // pub fn dedup(sa: StreamAdaptor) -> Result<StreamAdaptor, Error> {}
+    // pub fn unique(sa: StreamAdaptor) -> Result<StreamAdaptor, Error> {}
 }
