6b3d5c204b72c2b191478fd92dca8d66c88249fa
Mark LeMoine <thatsgobbles@gmail.com>
2019-06-07 10:37:22 -0700

    Created top-level `Op` enums for unary and binary functions

--- src/functions/operator/binary.rs
+++ src/functions/operator/binary.rs
@@ -8,3 +8,31 @@ pub use self::converter::Converter;
 pub use self::predicate::Predicate;
 pub use self::iter_consumer::IterConsumer;
 pub use self::iter_adaptor::IterAdaptor;
+
+#[derive(Clone, Copy, Debug)]
+pub enum Op {
+    Nth,
+    StepBy,
+    Chain,
+    Zip,
+    Map,
+    Filter,
+    SkipWhile,
+    TakeWhile,
+    Skip,
+    Take,
+    All,
+    Any,
+    Find,
+    Position,
+    Interleave,
+    Intersperse,
+    Chunks,
+    Windows,
+    Eq,
+    Ne,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+}
--- src/functions/operator/unary.rs
+++ src/functions/operator/unary.rs
@@ -8,3 +8,28 @@ pub use self::converter::Converter;
 pub use self::predicate::Predicate;
 pub use self::iter_consumer::IterConsumer;
 pub use self::iter_adaptor::IterAdaptor;
+
+use self::imp::Impl;
+
+use crate::functions::operand::Operand;
+use crate::functions::util::value_producer::ValueProducer;
+
+#[derive(Clone, Copy, Debug)]
+pub enum Op {
+    Collect,
+    Count,
+    First,
+    Last,
+    MinIn,
+    MaxIn,
+    Rev,
+    Sort,
+    Sum,
+    Prod,
+    AllEqual,
+    Flatten,
+    Dedup,
+    Unique,
+    Neg,
+    Abs,
+}
