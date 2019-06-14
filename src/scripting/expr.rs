aa45fb4ed0cfd280a9903b0cddc4f45cbccf6175
Mark LeMoine <thatsgobbles@gmail.com>
2019-06-14 13:38:33 -0700

    Replaced `.eval()` methods with `TryFrom` impls for `Expr`/`Thunk`

--- src/scripting/expr.rs
+++ src/scripting/expr.rs
@@ -7,6 +7,9 @@ pub use self::op::UnaryOp;
 pub use self::op::BinaryOp;
 pub use self::thunk::Thunk;
 
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
 use crate::scripting::Error;
 
 pub enum Expr<'e> {
@@ -14,11 +17,13 @@ pub enum Expr<'e> {
     Binary(BinaryOp, Thunk<'e>, Thunk<'e>),
 }
 
-impl<'e> Expr<'e> {
-    pub fn eval(self) -> Result<Arg<'e>, Error> {
-        match self {
-            Self::Unary(u_op, th) => u_op.process(th.eval()?),
-            Self::Binary(b_op, th_a, th_b) => b_op.process(th_a.eval()?, th_b.eval()?),
+impl<'e> TryFrom<Expr<'e>> for Arg<'e> {
+    type Error = Error;
+
+    fn try_from(e: Expr<'e>) -> Result<Self, Self::Error> {
+        match e {
+            Expr::Unary(u_op, th) => u_op.process(th.try_into()?),
+            Expr::Binary(b_op, th_a, th_b) => b_op.process(th_a.try_into()?, th_b.try_into()?),
         }
     }
 }
--- src/scripting/expr/thunk.rs
+++ src/scripting/expr/thunk.rs
@@ -1,3 +1,6 @@
+use std::convert::TryFrom;
+use std::convert::TryInto;
+
 use crate::scripting::expr::Expr;
 use crate::scripting::expr::arg::Arg;
 use crate::scripting::Error;
@@ -7,11 +10,14 @@ pub enum Thunk<'t> {
     Expr(Box<Expr<'t>>),
 }
 
-impl<'t> Thunk<'t> {
-    pub fn eval(self) -> Result<Arg<'t>, Error> {
-        match self {
-            Self::Arg(o) => Ok(o),
-            Self::Expr(e) => e.eval(),
+impl<'t> TryFrom<Thunk<'t>> for Arg<'t> {
+    type Error = Error;
+
+    fn try_from(t: Thunk<'t>) -> Result<Self, Self::Error> {
+        match t {
+            Thunk::Arg(o) => Ok(o),
+            // LEARN: Why is the dereference needed?
+            Thunk::Expr(e) => (*e).try_into(),
         }
     }
 }
