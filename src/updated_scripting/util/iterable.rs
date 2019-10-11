67b3322051130c7060264d7f32ddc391afbd6ae2
Mark LeMoine <thatsgobbles@gmail.com>
2019-10-11 13:49:00 -0700

    Added `first` and `last`, and about to add iteration to `Iterable`

--- src/updated_scripting/util/iterable.rs
+++ src/updated_scripting/util/iterable.rs
@@ -1,6 +1,8 @@
 
-use crate::metadata::types::MetaVal;
+use std::borrow::Cow;
 
+use crate::metadata::types::MetaVal;
+use crate::updated_scripting::Error;
 use crate::updated_scripting::util::Util;
 
 #[derive(Copy, Clone)]
@@ -13,6 +15,14 @@ pub enum Iterable<'a> {
     // Producer(ValueProducer<'a>),
 }
 
+impl<'a> Iterator for Iterable<'a> {
+    type Item = Result<Cow<'a, MetaVal>, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        None
+    }
+}
+
 impl<'a> Iterable<'a> {
     pub fn is_lazy(&self) -> bool {
         match self {
@@ -22,11 +32,7 @@ impl<'a> Iterable<'a> {
         }
     }
 
-    pub fn is_eager(&self) -> bool {
-        !self.is_lazy()
-    }
-
-    /// Counts the number of values contained in this iterable.
+    /// Counts the number of items contained in this iterable.
     pub fn count(self) -> usize {
         match self {
             Self::Borrowed(s) => s.len(),
@@ -34,7 +40,7 @@ impl<'a> Iterable<'a> {
         }
     }
 
-    /// Collects the contained values eagerly.
+    /// Collects the contained items eagerly.
     /// This is a no-op if this iterable is already collected.
     pub fn collect(self) -> Vec<MetaVal> {
         match self {
@@ -44,7 +50,9 @@ impl<'a> Iterable<'a> {
     }
 
     /// Helper method for `rev`/`sort`.
-    fn rev_sort(mut seq: Vec<MetaVal>, flag: RevSort) -> Vec<MetaVal> {
+    fn rev_sort(seq: Vec<MetaVal>, flag: RevSort) -> Vec<MetaVal> {
+        let mut seq = seq;
+
         match flag {
             // Reverse the slice mutably in-place.
             RevSort::Rev => seq.reverse(),
@@ -56,15 +64,37 @@ impl<'a> Iterable<'a> {
         seq
     }
 
-    /// Reverses the order of the values in this iterable.
-    /// Eagerly collects the values beforehand if not already collected.
+    /// Reverses the order of the items in this iterable.
+    /// Eagerly collects the items beforehand if not already collected.
     pub fn rev(self) -> Vec<MetaVal> {
         Self::rev_sort(self.collect(), RevSort::Rev)
     }
 
-    /// Sorts the values in this iterable using the default sort comparison.
-    /// Eagerly collects the values beforehand if not already collected.
+    /// Sorts the items in this iterable using the default sort comparison.
+    /// Eagerly collects the items beforehand if not already collected.
     pub fn sort(self) -> Vec<MetaVal> {
         Self::rev_sort(self.collect(), RevSort::Sort)
+    }
+
+    /// Returns the first item in this iterable, if there is one.
+    pub fn first(self) -> Option<Cow<'a, MetaVal>> {
+        match self {
+            Self::Borrowed(s) => s.first().map(Cow::Borrowed),
+            Self::Owned(s) => s.into_iter().next().map(Cow::Owned),
+        }
+    }
+
+    /// Returns the last item in this iterable, if there is one.
+    pub fn last(self) -> Option<Cow<'a, MetaVal>> {
+        match self {
+            Self::Borrowed(s) => s.last().map(Cow::Borrowed),
+            Self::Owned(s) => s.into_iter().last().map(Cow::Owned),
+        }
+    }
+
+    /// Checks if all items are equal to each other.
+    /// If empty, returns true.
+    pub fn all_equal(self) -> bool {
+        true
     }
 }
