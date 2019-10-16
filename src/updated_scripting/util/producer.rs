c92e1798695bbf970f36ab68f677ca81028b6bf4
Mark LeMoine <thatsgobbles@gmail.com>
2019-10-15 17:41:06 -0700

    Trying out dyn trait for `Producer` enum

--- src/updated_scripting/util/producer.rs
+++ src/updated_scripting/util/producer.rs
@@ -7,79 +7,38 @@ use crate::updated_scripting::Error;
 
 pub use self::producers::*;
 
-pub enum Producer<'a, I>
-where
-    I: Iterator<Item = Result<MetaVal, Error>>,
-{
-    Source(Source<'a>),
-    Fixed(Fixed),
-    Raw(Raw),
-    Flatten(Flatten<I>),
-}
+pub struct Producer(Box<dyn Iterator<Item = Result<MetaVal, Error>>>);
 
-impl<'a, I> From<Vec<MetaVal>> for Producer<'a, I>
-where
-    I: Iterator<Item = Result<MetaVal, Error>>,
-{
+impl From<Vec<MetaVal>> for Producer {
     fn from(v: Vec<MetaVal>) -> Self {
-        Self::Fixed(v.into())
+        Self(Box::new(Fixed::new(v)))
     }
 }
 
-impl<'a, I> From<Vec<Result<MetaVal, Error>>> for Producer<'a, I>
-where
-    I: Iterator<Item = Result<MetaVal, Error>>,
-{
+impl From<Vec<Result<MetaVal, Error>>> for Producer {
     fn from(v: Vec<Result<MetaVal, Error>>) -> Self {
-        Self::Raw(v.into())
+        Self(Box::new(Raw::new(v)))
     }
 }
 
-impl<'a, I> TryFrom<Producer<'a, I>> for Vec<MetaVal>
-where
-    I: Iterator<Item = Result<MetaVal, Error>>,
-{
+impl TryFrom<Producer> for Vec<MetaVal> {
     type Error = Error;
 
-    fn try_from(vp: Producer<'a, I>) -> Result<Self, Self::Error> {
-        vp.collect::<Result<Vec<_>, _>>()
+    fn try_from(prod: Producer) -> Result<Self, Self::Error> {
+        prod.0.collect::<Result<Vec<_>, _>>()
     }
 }
 
-impl<'a, I> From<Producer<'a, I>> for Vec<Result<MetaVal, Error>>
-where
-    I: Iterator<Item = Result<MetaVal, Error>>,
-{
-    fn from(vp: Producer<'a, I>) -> Self {
-        vp.collect()
+impl From<Producer> for Vec<Result<MetaVal, Error>> {
+    fn from(prod: Producer) -> Self {
+        prod.0.collect()
     }
 }
 
-impl<'a, I> Producer<'a, I>
-where
-    I: Iterator<Item = Result<MetaVal, Error>>,
-{
-    pub fn fixed(v: Vec<MetaVal>) -> Self {
-        Self::Fixed(Fixed::new(v))
-    }
-
-    pub fn raw(v: Vec<Result<MetaVal, Error>>) -> Self {
-        Self::Raw(Raw::new(v))
-    }
-}
-
-impl<'a, I> Iterator for Producer<'a, I>
-where
-    I: Iterator<Item = Result<MetaVal, Error>>,
-{
+impl Iterator for Producer {
     type Item = Result<MetaVal, Error>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        match self {
-            &mut Self::Source(ref mut s) => s.next(),
-            &mut Self::Fixed(ref mut s) => s.next(),
-            &mut Self::Raw(ref mut s) => s.next(),
-            &mut Self::Flatten(ref mut s) => s.next(),
-        }
+        self.0.next()
     }
 }
