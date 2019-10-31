09a436c718096acba0263627950db77ae422dfbd
Mark LeMoine <thatsgobbles@gmail.com>
2019-10-30 17:59:20 -0700

    Added `TryFrom<Arg>` impl for `MetaVal`

--- src/updated_scripting/arg.rs
+++ src/updated_scripting/arg.rs
@@ -1,6 +1,8 @@
 //! Represents an argument that is fully realized, i.e. not a delayed expression.
 //! These values need to be stable over time, as they can be used in partialled operators.
 
+use std::convert::TryFrom;
+
 use crate::metadata::types::MetaVal;
 use crate::updated_scripting::ops::Predicate;
 use crate::updated_scripting::ops::Converter;
@@ -18,5 +20,16 @@ pub enum Arg {
 impl From<MetaVal> for Arg {
     fn from(mv: MetaVal) -> Self {
         Arg::Value(mv)
+    }
+}
+
+impl TryFrom<Arg> for MetaVal {
+    type Error = ();
+
+    fn try_from(a: Arg) -> Result<Self, Self::Error> {
+        match a {
+            Arg::Value(mv) => Ok(mv),
+            _ => Err(()),
+        }
     }
 }
