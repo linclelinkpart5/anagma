e947ac7ad8d06f2fe280c53c0d1f0336e72b8cf2
Mark LeMoine <thatsgobbles@gmail.com>
2019-11-11 17:40:17 -0800

    Defining `Converter` variants

--- src/updated_scripting/ops/converter.rs
+++ src/updated_scripting/ops/converter.rs
@@ -1,4 +1,7 @@
 
+use std::collections::BTreeMap;
+
+use crate::util::Number;
 use crate::metadata::types::MetaVal;
 use crate::updated_scripting::Error;
 use crate::updated_scripting::ops::predicate::Predicate;
@@ -25,6 +28,45 @@ pub enum Converter {
     Pick,
     Load,
     Predicate(Predicate),
+
+    NthA(usize),
+    NthB(Vec<MetaVal>),
+    All(Predicate),
+    Any(Predicate),
+    Find(Predicate),
+    Pos(Predicate),
+    Filter(Predicate),
+    SkipWhile(Predicate),
+    TakeWhile(Predicate),
+    Map(Box<Converter>),
+    SkipA(usize),
+    SkipB(Vec<MetaVal>),
+    TakeA(usize),
+    TakeB(Vec<MetaVal>),
+    StepByA(usize),
+    StepByB(Vec<MetaVal>),
+    ChainA(Vec<MetaVal>),
+    ChainB(Vec<MetaVal>),
+    ZipA(Vec<MetaVal>),
+    ZipB(Vec<MetaVal>),
+    IntersperseA(MetaVal),
+    IntersperseB(Vec<MetaVal>),
+    RoundRobinA(Vec<MetaVal>),
+    RoundRobinB(Vec<MetaVal>),
+    Add(Number),
+    SubA(Number),
+    SubB(Number),
+    Mul(Number),
+    DivA(Number),
+    DivB(Number),
+    RemA(Number),
+    RemB(Number),
+    LookupA(String),
+    LookupB(BTreeMap<String, MetaVal>),
+    SaveA(MetaVal),
+    SaveB(String),
+    Min(Number),
+    Max(Number),
 }
 
 impl Converter {
