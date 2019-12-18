cff68deb75f5ee9e1df8453ac567e8ac6b874a6f
Mark LeMoine <thatsgobbles@gmail.com>
2019-12-17 17:24:51 -0800

    Polished `metadata::block`

--- src/metadata/block.rs
+++ src/metadata/block.rs
@@ -1,11 +1,15 @@
-//! Types for modeling and representing item metadata.
-
-use std::collections::BTreeMap;
+//! Types for modeling and representing metadata for one or more items.
 
 use indexmap::IndexMap;
 
-use crate::metadata::value::Value;
+use crate::metadata::value::Mapping;
 
-pub type Block = BTreeMap<String, Value>;
+/// Represents a chunk of metadata for one item.
+// NOTE: This alias is intentional, this is taken advantage of later downstream.
+pub type Block = Mapping;
+
+/// Represents multiple chunks of metadata for an ordered collection of items.
 pub type BlockSeq = Vec<Block>;
+
+/// Represents multiple chunks of metadata for a mapping of items keyed by name.
 pub type BlockMap = IndexMap<String, Block>;
