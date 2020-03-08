305a79a1ec5b896df5c73da1f4bae88f58073f22
Mark LeMoine <thatsgobbles@gmail.com>
2020-03-07 16:53:01 -0800

    First attempt at more ergonomic re-exports

--- src/metadata/mod.rs
+++ src/metadata/mod.rs
@@ -8,47 +8,10 @@ pub mod block;
 pub mod value;
 pub mod schema;
 
-use crate::metadata::processor::Error as ProcessorError;
+pub use self::value::{Value, Error as ValueError};
 
-#[derive(Debug)]
-pub enum Error {
-    Processor(ProcessorError),
-}
+pub use self::block::{Block, BlockSequence, BlockMapping};
 
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        match *self {
-            Error::Processor(ref err) => write!(f, "cannot process metadata: {}", err),
-        }
-    }
-}
+pub use self::target::{Target, Error as TargetError};
 
-impl std::error::Error for Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match *self {
-            Error::Processor(ref err) => Some(err),
-        }
-    }
-}
-
-// pub struct Metadata;
-
-// impl Metadata {
-//     pub fn get_metadata<'k, P: AsRef<Path>>(item_path: P) -> Result<Block<'k>, Error> {
-//         // Use a default configuration and no aggregations.
-//         let config = Config::default();
-
-//         Self::get_metadata_with_config(item_path, &config)
-//     }
-
-//     pub fn get_metadata_with_config<P: AsRef<Path>>(item_path: P, config: &Config) -> Result<Block, Error> {
-//         let mb = Processor::process_item_file(
-//             item_path,
-//             config.meta_format,
-//             &config.selection,
-//             config.sort_order,
-//         ).map_err(Error::Processor)?;
-
-//         Ok(mb)
-//     }
-// }
+pub use self::processor::Error as ProcessorError;
