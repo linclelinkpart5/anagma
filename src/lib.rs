0061204567d648749fa98cc87c87107baedc4d76
Mark LeMoine <linclelinkpart5@gmail.com>
2020-10-13 22:01:54 -0700

    Adding module-level `get`/`get_with_config` methods

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,23 @@ pub mod config;
 mod util;
 
 #[cfg(test)] mod test_util;
+
+use std::path::Path;
+
+use crate::config::Config;
+use crate::metadata::block::Block;
+use crate::metadata::processor::Processor;
+
+pub fn get<P: AsRef<Path>>(path: &P) -> Block {
+    let config = Config::default();
+    get_with_config(path, &config)
+}
+
+pub fn get_with_config<P: AsRef<Path>>(path: &P, config: &Config) -> Block {
+    Processor::process_item_file(
+        path.as_ref(),
+        &config.schema_format,
+        &config.selection,
+        &config.sorter,
+    ).unwrap()
+}
