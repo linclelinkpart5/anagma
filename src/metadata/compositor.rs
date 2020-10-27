5a08e969839a81a4587e565fff81257b71ba9a24
Mark LeMoine <linclelinkpart5@gmail.com>
2020-10-27 10:01:10 -0700

    Making `Source` hold `OsString`, and including `SchemaFormat` in `Compositor`

--- src/metadata/compositor.rs
+++ src/metadata/compositor.rs
@@ -1,19 +1,44 @@
 
+use std::ffi::OsString;
+
 use crate::metadata::schema::SchemaFormat;
 
-enum Source<'a> {
+pub(crate) enum Source {
     /// The metadata file location is a sibling of the target item file path.
-    External(&'a str),
+    External(OsString),
 
     /// The metadata file location is inside the target item file path.
     /// Implies that the the target item file path is a directory.
-    Internal(&'a str),
+    Internal(OsString),
 }
 
-pub struct Compositor<'a>(Vec<Source<'a>>);
+pub struct Compositor(Vec<Source>, SchemaFormat);
 
-impl<'a> Compositor<'a> {
-    pub fn new<S: AsRef<str>>() -> Self {
-        Self(Vec::new())
+impl<'a> Compositor {
+    pub(crate) fn new(fmt: SchemaFormat) -> Self {
+        Self(Vec::new(), fmt)
+    }
+
+    fn _add_src<I, F>(&mut self, file_stub: I, f: F) -> &mut Self
+    where
+        I: Into<OsString>,
+        F: Fn(OsString) -> Source,
+    {
+        let mut src_fn = file_stub.into();
+        src_fn.push(".");
+        src_fn.push(self.1.file_extension());
+
+        let src = f(src_fn);
+
+        self.0.push(src);
+        self
+    }
+
+    pub(crate) fn ex_source<I: Into<OsString>>(&mut self, file_stub: I) -> &mut Self {
+        self._add_src(file_stub, Source::External)
+    }
+
+    pub(crate) fn in_source<I: Into<OsString>>(&mut self, file_stub: I) -> &mut Self {
+        self._add_src(file_stub, Source::Internal)
     }
 }
