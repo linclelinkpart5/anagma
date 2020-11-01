d99ff211ad824eeea25c9778fcc5e202696f857f
Mark LeMoine <linclelinkpart5@gmail.com>
2020-10-31 20:16:55 -0700

    Deleting `SourcerMetaPaths`, as it didn't make sense, deleted `sourcer`, and simplified naming in `source`

--- src/source.rs
+++ src/source.rs
@@ -1,5 +1,4 @@
 use std::borrow::Cow;
-use std::ffi::OsString;
 use std::io::{Error as IoError, ErrorKind as IoErrorKind, Result as IoResult};
 use std::path::{Path, PathBuf};
 
@@ -11,17 +10,7 @@ use crate::util::NameError;
 use crate::util::Util;
 
 #[derive(Debug, Error)]
-pub enum SourceError {
-    #[error("source file name is invalid: {0}")]
-    InvalidName(String),
-    #[error("source file name does not have an extension: {0}")]
-    EmptyExtension(String),
-    #[error("unknown format for file extension: {}", .0.to_string_lossy())]
-    UnknownExtension(OsString),
-}
-
-#[derive(Debug, Error)]
-pub enum LookupError {
+pub enum Error {
     #[error("not a directory: {}", .0.display())]
     NotADir(PathBuf),
     #[error("not a file: {}", .0.display())]
@@ -39,12 +28,11 @@ pub enum LookupError {
 
     #[error("unable to read item directory: {0}")]
     IterDir(#[source] IoError),
-    #[error("unable to read item directory entry: {0}")]
-    IterDirEntry(#[source] IoError),
-    // Bulk(IoError, Vec<IoError>),
+    // #[error("unable to read item directory entry: {0}")]
+    // IterDirEntry(#[source] IoError),
 }
 
-impl LookupError {
+impl Error {
     pub(crate) fn is_fatal(&self) -> bool {
         match self {
             Self::MetaAccess(_, io_error) => match io_error.kind() {
@@ -117,21 +105,21 @@ impl Source {
 
     /// Given a concrete item file path, returns the meta file path that would
     /// provide metadata for that item path, according to the source rules.
-    pub fn meta_path(&self, item_path: &Path) -> Result<PathBuf, LookupError> {
+    pub fn meta_path(&self, item_path: &Path) -> Result<PathBuf, Error> {
         // Get filesystem stat for item path.
         // This step is always done, even if the file/directory status does not
         // need to be checked, as it provides useful error information about
         // permissions and non-existence.
         let item_fs_stat = std::fs::metadata(&item_path)
-            .map_err(|io| LookupError::ItemAccess(item_path.into(), io))?;
+            .map_err(|io| Error::ItemAccess(item_path.into(), io))?;
 
         let meta_path_parent_dir = match self.anchor {
             Anchor::External => item_path
                 .parent()
-                .ok_or_else(|| LookupError::NoItemParentDir(item_path.into()))?,
+                .ok_or_else(|| Error::NoItemParentDir(item_path.into()))?,
             Anchor::Internal => {
                 if !item_fs_stat.is_dir() {
-                    return Err(LookupError::NotADir(item_path.into()));
+                    return Err(Error::NotADir(item_path.into()));
                 }
 
                 item_path
@@ -143,16 +131,15 @@ impl Source {
 
         // Get filesystem stat for meta path.
         // NOTE: Using `match` in order to avoid a clone in the error case.
-        // TODO: Move fatal error determination logic here.
         let meta_fs_stat = match std::fs::metadata(&meta_path) {
             Ok(o) => o,
-            Err(io_err) => return Err(LookupError::MetaAccess(meta_path, io_err)),
+            Err(io_err) => return Err(Error::MetaAccess(meta_path, io_err)),
         };
 
         // Ensure that the meta path is indeed a file.
         if !meta_fs_stat.is_file() {
             // Found a directory with the meta file name, this would be an unusual error case.
-            Err(LookupError::NotAFile(meta_path))
+            Err(Error::NotAFile(meta_path))
         } else {
             Ok(meta_path)
         }
@@ -162,12 +149,12 @@ impl Source {
     /// could/should provide metadata for. Note that this does NOT parse meta
     /// files, it only uses file system locations and presence. In addition, no
     /// filtering or sorting of the returned item paths is performed.
-    pub fn item_paths<'a>(&self, meta_path: &'a Path) -> Result<ItemPaths<'a>, LookupError> {
+    pub fn item_paths<'a>(&self, meta_path: &'a Path) -> Result<ItemPaths<'a>, Error> {
         let meta_fs_stat = std::fs::metadata(&meta_path)
-            .map_err(|io| LookupError::MetaAccess(meta_path.into(), io))?;
+            .map_err(|io| Error::MetaAccess(meta_path.into(), io))?;
 
         if !meta_fs_stat.is_file() {
-            return Err(LookupError::NotAFile(meta_path.into()));
+            return Err(Error::NotAFile(meta_path.into()));
         }
 
         // Get the parent directory of the meta file.
@@ -176,7 +163,7 @@ impl Source {
                 Anchor::External => {
                     // Return all children of the parent directory of this meta file.
                     let read_dir =
-                        std::fs::read_dir(&meta_parent_dir_path).map_err(LookupError::IterDir)?;
+                        std::fs::read_dir(&meta_parent_dir_path).map_err(Error::IterDir)?;
 
                     ItemPathsInner::ReadDir(read_dir)
                 }
@@ -191,7 +178,7 @@ impl Source {
             // This should never happen, since at this point we have a real meta
             // file and thus, a real parent directory for that file, but making
             // an error for it anyways.
-            Err(LookupError::NoMetaParentDir(meta_path.into()))
+            Err(Error::NoMetaParentDir(meta_path.into()))
         }
     }
 
@@ -201,48 +188,13 @@ impl Source {
         &self,
         meta_path: &'a Path,
         selection: &'a Selection,
-    ) -> Result<SelectedItemPaths<'a>, LookupError> {
+    ) -> Result<SelectedItemPaths<'a>, Error> {
         Ok(SelectedItemPaths(self.item_paths(meta_path)?, selection))
     }
 }
 
-// pub struct OldSourcer(Vec<Source>, SchemaFormat);
-
-// impl OldSourcer {
-//     pub fn new(sources: Vec<Source>, format: SchemaFormat) -> Self {
-//         Self(sources, format)
-//     }
-
-//     pub fn from_stubs(stubs: Vec<String>, format: SchemaFormat) -> Result<Self, NameError> {
-//         for stub in stubs {
-//             // Ensure that the stub is a valid file name.
-//             let mut stub = Util::validate_item_name(stub)?;
-
-//             // It is expected that appending a suffix at the end will still keep
-//             // a valid file name.
-//             stub.push('.');
-//             stub.push_str(format.file_extension());
-//         }
-
-//         let mut sources = Vec::new();
-
-//         Ok(Self(sources, format))
-//     }
-
-//     pub fn from_sources(sources: Vec<Source>) -> Result<Self, ()> {
-//         // Try and automatically detect format by looking at the sources.
-//         // All sources must have either no extension or a known format
-//         // extension, at least one source must have a known format extension,
-//         // and all sources that have an extension must have the same extension.
-
-//         todo!()
-//     }
-// }
-
-// Only strings, the schema format is only used to convert file stubs into
-// file names. Since the responsibility of this type is only to produce
-// candidate meta paths, and not actually parse them, this does not need to
-// store schema formats.
+// Represents an ordered collection of `Source`s, designed to find meta files
+// for a target item path.
 pub struct Sourcer(Vec<Source>);
 
 impl Sourcer {
@@ -254,79 +206,11 @@ impl Sourcer {
         self.0.push(source);
         self
     }
-}
 
-pub struct SourcerMetaPaths<'a> {
-    iter: std::slice::Iter<'a, Source>,
-    item_path: &'a Path,
-}
-
-impl<'a> Iterator for SourcerMetaPaths<'a> {
-    type Item = Result<PathBuf, LookupError>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        while let Some(source) = self.iter.next() {
-            let res = source.meta_path(self.item_path);
-
-            match res {
-                Ok(meta_path) => {
-                    return Some(Ok(meta_path));
-                }
-                Err(err) if err.is_fatal() => {
-                    return Some(Err(err));
-                }
-                Err(_) => {
-                    continue;
-                }
-            }
-        }
-
-        None
-    }
-}
-
-pub struct SourcerItemPaths<'a> {
-    source_iter: std::slice::Iter<'a, Source>,
-    meta_path: &'a Path,
-    curr_item_path_iter: Option<ItemPaths<'a>>,
-}
-
-impl<'a> Iterator for SourcerItemPaths<'a> {
-    type Item = Result<Cow<'a, Path>, LookupError>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            // If there is no current item path iterator, pull the next source
-            // and create one.
-            if self.curr_item_path_iter.is_none() {
-                // If there are no more sources, this entire iterator is done.
-                let src = self.source_iter.next()?;
-                let it = match src.item_paths(&self.meta_path) {
-                    Err(err) => {
-                        return Some(Err(err));
-                    }
-                    Ok(it) => it,
-                };
-
-                self.curr_item_path_iter = Some(it);
-            }
-
-            let item_path_iter = self.curr_item_path_iter.as_mut()?;
-
-            match item_path_iter.next() {
-                Some(Err(io_err)) => {
-                    return Some(Err(LookupError::IterDirEntry(io_err)));
-                },
-                Some(Ok(item_path)) => {
-                    return Some(Ok(item_path));
-                },
-                None => {
-                    // This item path iter has been exhausted, drop it and
-                    // restart the loop.
-                    self.curr_item_path_iter = None;
-                    continue;
-                },
-            }
+    pub fn meta_paths<'a>(&'a self, item_path: &'a Path) -> MetaPaths<'a> {
+        MetaPaths {
+            iter: self.0.iter(),
+            item_path,
         }
     }
 }
@@ -379,6 +263,35 @@ impl<'a> Iterator for SelectedItemPaths<'a> {
                         return Some(Err(err));
                     }
                 },
+            }
+        }
+
+        None
+    }
+}
+
+pub struct MetaPaths<'a> {
+    iter: std::slice::Iter<'a, Source>,
+    item_path: &'a Path,
+}
+
+impl<'a> Iterator for MetaPaths<'a> {
+    type Item = Result<PathBuf, Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(source) = self.iter.next() {
+            let res = source.meta_path(self.item_path);
+
+            match res {
+                Ok(meta_path) => {
+                    return Some(Ok(meta_path));
+                }
+                Err(err) if err.is_fatal() => {
+                    return Some(Err(err));
+                }
+                Err(_) => {
+                    continue;
+                }
             }
         }
 
