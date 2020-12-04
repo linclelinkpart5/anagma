766a9061d1016b3c612241af703239a06b5c3630
Mark LeMoine <linclelinkpart5@gmail.com>
2020-12-03 22:09:31 -0800

    Adding more "library" methods and iterators to `Block`

--- src/types/block.rs
+++ src/types/block.rs
@@ -1,7 +1,14 @@
 use std::iter::FusedIterator;
 use std::collections::BTreeMap as InnerMap;
-use std::collections::btree_map::{IntoIter as InnerIntoIter, Iter as InnerIter};
-use std::iter::FromIterator;
+use std::collections::btree_map::{
+    IntoIter as InnerIntoIter,
+    Iter as InnerIter,
+    IterMut as InnerIterMut,
+    Keys as InnerKeys,
+    Values as InnerValues,
+    ValuesMut as InnerValuesMut,
+};
+use std::iter::{Extend, FromIterator};
 
 use crate::types::Value;
 
@@ -14,8 +21,32 @@ impl Block {
         Self::default()
     }
 
+    pub fn clear(&mut self) {
+        self.0.clear()
+    }
+
+    pub fn insert(&mut self, key: String, value: Value) -> Option<Value> {
+        self.0.insert(key, value)
+    }
+
+    pub fn contains_key(&self, key: &str) -> bool {
+        self.0.contains_key(key)
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
     pub fn get(&self, key: &str) -> Option<&Value> {
         self.0.get(key)
+    }
+
+    pub fn get_mut(&mut self, key: &str) -> Option<&mut Value> {
+        self.0.get_mut(key)
     }
 
     pub fn get_full(&self, key: &str) -> Option<(&String, &Value)> {
@@ -32,6 +63,28 @@ impl Block {
 
     pub fn iter(&self) -> Iter<'_> {
         Iter(self.0.iter())
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<'_> {
+        IterMut(self.0.iter_mut())
+    }
+
+    pub fn keys(&self) -> Keys<'_> {
+        Keys(self.0.keys())
+    }
+
+    pub fn values(&self) -> Values<'_> {
+        Values(self.0.values())
+    }
+
+    pub fn values_mut(&mut self) -> ValuesMut<'_> {
+        ValuesMut(self.0.values_mut())
+    }
+}
+
+impl Extend<(String, Value)> for Block {
+    fn extend<I: IntoIterator<Item = (String, Value)>>(&mut self, iter: I) {
+        self.0.extend(iter)
     }
 }
 
@@ -80,6 +133,118 @@ impl<'a> ExactSizeIterator for Iter<'a> {
 }
 
 impl<'a> FusedIterator for Iter<'a> {}
+
+pub struct IterMut<'a>(InnerIterMut<'a, String, Value>);
+
+impl<'a> Iterator for IterMut<'a> {
+    type Item = (&'a String, &'a mut Value);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl<'a> DoubleEndedIterator for IterMut<'a> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.next_back()
+    }
+}
+
+impl<'a> ExactSizeIterator for IterMut<'a> {
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl<'a> FusedIterator for IterMut<'a> {}
+
+pub struct Keys<'a>(InnerKeys<'a, String, Value>);
+
+impl<'a> Iterator for Keys<'a> {
+    type Item = &'a String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl<'a> DoubleEndedIterator for Keys<'a> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.next_back()
+    }
+}
+
+impl<'a> ExactSizeIterator for Keys<'a> {
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl<'a> FusedIterator for Keys<'a> {}
+
+pub struct Values<'a>(InnerValues<'a, String, Value>);
+
+impl<'a> Iterator for Values<'a> {
+    type Item = &'a Value;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl<'a> DoubleEndedIterator for Values<'a> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.next_back()
+    }
+}
+
+impl<'a> ExactSizeIterator for Values<'a> {
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl<'a> FusedIterator for Values<'a> {}
+
+pub struct ValuesMut<'a>(InnerValuesMut<'a, String, Value>);
+
+impl<'a> Iterator for ValuesMut<'a> {
+    type Item = &'a mut Value;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl<'a> DoubleEndedIterator for ValuesMut<'a> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        self.0.next_back()
+    }
+}
+
+impl<'a> ExactSizeIterator for ValuesMut<'a> {
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl<'a> FusedIterator for ValuesMut<'a> {}
 
 pub struct IntoIter(InnerIntoIter<String, Value>);
 
